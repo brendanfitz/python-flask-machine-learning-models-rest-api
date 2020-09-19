5248575ced19cba18c2a47242c35fa7bfd495e7d
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2020-09-18 21:23:20 -0400

    Move movie ROI prediction logic to MovieRoiPredictor class

--- prediction_api/__init__.py
+++ prediction_api/__init__.py
@@ -14,36 +14,10 @@ def hello():
 
 @app.route('/movie_roi')
 def movie_roi():
-    args = request.args
-    print(args)
-
-    budget = request.args.get('budget')
-    budget_scaled = movie_mod.budget_poly.transform([[budget]])
-    budget_df = movie_mod.budget_poly_scaler.transform(budget_scaled)
-
-    passthroughs_df = movie_mod.passthroughs_scaler.transform([
-       [
-           request.args.get('in_release_days'),
-           request.args.get('widest_release'),
-           request.args.get('runtime'),
-       ],
-    ])
-
-    rating = request.args.get('rating')
-    rating_df = movie_mod.ohe.transform([[rating]]).toarray()
-
-    genre = request.args.get('genre')
-    genre_df = movie_mod.cv.transform([genre]).toarray()
-
-    frames = [budget_df, passthroughs_df, rating_df, genre_df]
-    row = np.concatenate(frames, axis=1)
-
-    prediction = movie_mod.regr.predict(row)[0]
-
+    prediction = movie_mod.predict(request.args)
     data = {
         'prediction': prediction,
     }
-
     return jsonify(data)
 
 @app.route('/lending_club_loan_default')
--- prediction_api/movie_roi_predictor.py
+++ prediction_api/movie_roi_predictor.py
@@ -1,4 +1,5 @@
 from os import path
+from numpy import concatenate
 from prediction_api.predictor import Predictor
 
 class MovieRoiPredictor(Predictor):
@@ -12,3 +13,29 @@ class MovieRoiPredictor(Predictor):
         self.ohe = self.load_pickle('ohe.pkl')
         self.cv = self.load_pickle('cv.pkl')
         self.passthroughs_scaler = self.load_pickle('passthroughs_scaler.pkl')
+    
+    def predict(self, args):
+        budget = args.get('budget')
+        budget_scaled = self.budget_poly.transform([[budget]])
+        budget_df = self.budget_poly_scaler.transform(budget_scaled)
+
+        passthroughs_df = self.passthroughs_scaler.transform([
+           [
+               args.get('in_release_days'),
+               args.get('widest_release'),
+               args.get('runtime'),
+           ],
+        ])
+
+        rating = args.get('rating')
+        rating_df = self.ohe.transform([[rating]]).toarray()
+
+        genre = args.get('genre')
+        genre_df = self.cv.transform([genre]).toarray()
+
+        frames = [budget_df, passthroughs_df, rating_df, genre_df]
+        row = concatenate(frames, axis=1)
+
+        prediction = self.regr.predict(row)[0]
+        
+        return prediction
